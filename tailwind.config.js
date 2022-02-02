f0a8a1496171c6c63af11aa13fd748732e21b1b8
MinJun Choi <mj043000@naver.com>
2022-02-02 20:50:35 +0900

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,44 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      typography: {
-        
-      }
+      typography: { (theme) => ({
+        DEFAULT: {
+          css: {
+            h1: {
+              color: '#0F4C81'
+            },
+            h2: {
+              color: '#0F4C81'
+            },
+            h3: 
+            {color: '#0F4C81'
+          },
+            h4: {
+              color: '#0F4C81'
+            },
+          }
+        }
+      })
     },
+    borderWidth: {
+      DEFAULT: '0.5px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '5': '5px',
+    },
+    fill: theme => ({
+      'red': theme('colors.red.500'),
+      'blue': theme('colors.blue.500'),
+      'green': theme('colors.green.500'),
+    })
   },
   variants: {
-    extend: {},
+    extend: {
+      translate: ['active', 'group-hover'],
+      transitionDuration: ['hover', 'focus'],
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
