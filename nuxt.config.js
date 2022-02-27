c3ea9eb6a74934f5aa49b30cba25fcf77e7b6999
MinJun Choi <mj043000@naver.com>
2022-02-27 18:32:38 +0900

    og image added

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,50 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Hi, there! I\'m MinJun Choi' },
+      { name: 'format-detection', content: 'telephone=no' },
+      // Twitter
+      // Test on: https://cards-dev.twitter.com/validator
+      {
+        hid: 't-type',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      // Open Graph
+      // Test on: https://developers.facebook.com/tools/debug/
+      { hid: 'og:site_name', property: 'og:site_name', content: 'MinJun Choi' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://choiminjun.com/'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'MinJun Choi'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'Hi, there! I\'m MinJun Choi'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://raw.githubusercontent.com/minjun0430/main/static/og_image.jpg'
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: 'https://raw.githubusercontent.com/minjun0430/main/static/og_image.jpg'
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'MinJun Choi Main Image'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
