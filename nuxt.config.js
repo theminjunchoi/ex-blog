9ffb1384dc986a9c2810ad5ebbdfdabc619415a7
MinJun Choi <mj043000@naver.com>
2022-03-28 13:55:19 +0900

    mobile favicon test

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon1.ico' }
     ]
   },
 
