96e3a838233cee56b0ca9c0e63520ecae486ee20
MinJun Choi <mj043000@naver.com>
2022-07-19 03:38:08 +0900

    change favicon

--- data/projects.js
+++ data/projects.js
@@ -9,7 +9,26 @@ const projectsData = [
     tech2: "TailwindCSS",
     tech3: "Netlify",
   },
-  
+  {
+    title: "GDSC CAUCON page",
+    description: `Page for GDSC CAUCON`,
+    imgSrc: "",
+    href: "https://gdsc-cau.github.io/CAUCON/",
+    github: "https://github.com/GDSC-CAU/CAUCON",
+    tech1: "Nuxt",
+    tech2: "TailwindCSS",
+    tech3: "Netlify",
+  },
+  {
+    title: "ZeroPage portal page",
+    description: `Page for ZeroPage newbie and advertising`,
+    imgSrc: "",
+    href: "https://zp-portal.org/",
+    github: "https://github.com/ZeroPage/ZP-portal-page",
+    tech1: "Nuxt",
+    tech2: "TailwindCSS",
+    tech3: "Netlify",
+  },
 ];
 
 export default projectsData;
