9fbfbce64a47e62c7cf00a5f7e202e11d93b289e
MinJun Choi <mj043000@naver.com>
2022-09-09 01:46:14 +0900

    minor chnage

--- pages/study/index.vue
+++ pages/study/index.vue
@@ -43,7 +43,20 @@ export default {
         return { studies }
     },
     name: 'PageIndex',
-    mixins: [aosMixin]
+    mixins: [aosMixin],
+    head: {
+    title: "MinJun Choi | Study",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Some of Projects developed by MinJun Choi",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
 }
 </script>
 
